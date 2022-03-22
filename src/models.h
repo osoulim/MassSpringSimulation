44063c899f2c25c534af500c22a051b113f43bb2
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2022-03-22 17:02:48 -0600

    Add some bullshit on git to work in home

--- src/models.h
+++ src/models.h
@@ -3,93 +3,109 @@
 #include <glm/glm.hpp>
 
 #include <vector>
+#include <memory>
 
 namespace simulation {
 
-using vec2f = glm::vec2;
-using vec3f = glm::vec3;
+	using vec2f = glm::vec2;
+	using vec3f = glm::vec3;
 
-struct Model {
-  virtual ~Model() = default;
-  virtual void reset() = 0;
-  virtual void step(float dt) = 0;
-};
+	struct Model {
+		virtual ~Model() = default;
+		virtual void reset() = 0;
+		virtual void step(float dt) = 0;
+	};
 
-struct Particle {
-  explicit Particle(vec3f position) : x(position) {}
-  Particle(vec3f position, vec3f velocity) : x(position), v(velocity) {}
+	struct Particle {
+		explicit Particle(vec3f position) : position(position) {}
+		Particle(vec3f position, vec3f velocity) : position(position), velocity(velocity) {}
 
-  vec3f x;
-  vec3f v = vec3f{0.f};
-};
+		vec3f position;
+		vec3f velocity = vec3f{0.f};
+		vec3f force = vec3f{0.f};
+		float mass = 1.f;
+		bool isStationary;
+	};
+
+	struct Spring {
+		std::shared_ptr<Particle> head;
+		std::shared_ptr<Particle> tail;
+	};
 
 //
 // Small angle pendulum
 //
-class SmallAnglePendulumModel : public Model {
-public:
-  SmallAnglePendulumModel();
-  void reset() override;
-  void step(float dt) override;
+	class SmallAnglePendulumModel : public Model {
+	public:
+		SmallAnglePendulumModel();
 
-public:
-  // constants
-  float const gravity = 9.81f;
-  float const theta0 = 0.5f;
-  float const armLength = 5.f;
-  float const mass = 1.f;
+		void reset() override;
 
-  // dependent variables
-  float t = 0.f;
-  float theta = theta0;
-};
+		void step(float dt) override;
+
+	public:
+		// constants
+		float const gravity = 9.81f;
+		float const theta0 = 0.5f;
+		float const armLength = 5.f;
+		float const mass = 1.f;
+
+		// dependent variables
+		float t = 0.f;
+		float theta = theta0;
+	};
+
 // free functions
-float smallAnglePendulum(float t, float theta0, float l, float mass,
-                         float graivty);
+	float smallAnglePendulum(float t, float theta0, float l, float mass,
+							 float graivty);
 
-vec3f pendulumPosition(float theta, float l);
+	vec3f pendulumPosition(float theta, float l);
 
 //
 // Double Pendulum
 //
-class DoublePendulumModel : public Model {
-public:
-  DoublePendulumModel();
+	class DoublePendulumModel : public Model {
+	public:
+		DoublePendulumModel();
 
-  void reset() override;
-  void step(float dt) override;
+		void reset() override;
 
-  vec3f mass0Position() const;
-  vec3f mass1Position() const;
+		void step(float dt) override;
 
-public:
-  // constants
-  float const g = 9.81f;
-  float const l = 10.f; // arm lengths
-  float const m = 1.f;  // mass
+		vec3f mass0Position() const;
 
-  // dependent variables
-  // angle
-  float theta0 = 0.f;
-  float theta1 = 0.f;
+		vec3f mass1Position() const;
 
-  // momentum
-  float p0 = 0.f;
-  float p1 = 0.f;
-};
+	public:
+		// constants
+		float const g = 9.81f;
+		float const l = 10.f; // arm lengths
+		float const m = 1.f;  // mass
+
+		// dependent variables
+		// angle
+		float theta0 = 0.f;
+		float theta1 = 0.f;
+
+		// momentum
+		float p0 = 0.f;
+		float p1 = 0.f;
+	};
 
 //
 // Particle Model
 //
-class ParticleModel : public Model {
-public:
-  ParticleModel();
-  void reset() override;
-  void step(float dt) override;
+	class ParticleModel : public Model {
+	public:
+		ParticleModel();
 
-public:
-  std::vector<Particle> particles;
-  float bounds = 10.f;
-};
+		void reset() override;
+
+		void step(float dt) override;
+
+	public:
+		std::vector<Particle> particles;
+		float bounds = 10.f;
+	};
 
 } // namespace simulation
